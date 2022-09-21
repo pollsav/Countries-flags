73d16549824419747de8a8fcbb52cd98513a3d95
pollsav <vladislovetexts@gmail.com>
2022-09-21 22:41:17 +0300

    logic,layout,styles

---
+++ src/store/controls/controls-actions.js
@@ -0,0 +1,15 @@
+export const SET_SEARCH = '@@controls/SET_SEARCH';
+export const SET_REGION = '@@controls/SET_REGION';
+export const SET_CLEAR = '@@controls/SET_CLEAR';
+
+export const setSearch = (search) => ({
+    type: SET_SEARCH,
+    payload: search,
+});
+export const setRegion = (region) => ({
+    type: SET_REGION,
+    payload: region,
+});
+export const clearControls = () => ({
+    type: SET_CLEAR,
+});
---
+++ src/store/details/details-actions.js
@@ -0,0 +1,40 @@
+export const SET_LOADING = '@@details/SET_LOADING';
+export const SET_ERROR = '@@details/SET_ERROR';
+export const SET_COUNTRY = '@@details/SET_COUNTRY';
+export const CLEAR_DETAILS = '@@details/CLEAR_DETAILS';
+export const SET_NEIGHBORS = '@@details/SET_NEIGHBORS';
+
+export const setLoading = () => ({
+    type: SET_LOADING
+});
+export const setErr = (err) => ({
+    type: SET_ERROR,
+    payload: err,
+});
+export const setCountry = (country) => ({
+    type: SET_COUNTRY,
+    payload: country,
+});
+export const clearDetails = () => ({
+    type: CLEAR_DETAILS,
+});
+const setNeighbors = (countries) => ({
+    type: SET_NEIGHBORS,
+    payload: countries,
+});
+
+export const loadCountryByName = (name) => (dispatch, _, {client, api}) => {
+    dispatch(setLoading());
+    client.get(api.searchByCountry(name))
+        .then(({data}) => {
+            dispatch(setCountry(data[0]))
+        })
+        .catch((err) => dispatch(setErr(err.message)))
+};
+export const loadNeighborsByBorder = (borders) => (dispatch, _, {client, api}) => {
+    client.get(api.filterByCode(borders))
+        .then(({data}) => {
+            dispatch(setNeighbors(data.map((c) => c.name)))
+        })
+        .catch(console.error)
+}
